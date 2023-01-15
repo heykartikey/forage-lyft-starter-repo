412385a0774a3e77f80aeeba50ae1955f467808c
Kartikey <kartikeysahoo@gmail.com>
2023-01-15 12:56:02 +0530

    It makes more sense to make these methods static, they don't need a CarFactory instance to work

--- car_factory.py
+++ car_factory.py
@@ -5,31 +5,37 @@ import battery
 
 
 class CarFactory:
-    def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    
+    @staticmethod
+    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
         return Car(
             engine.CapuletEngine(current_mileage, last_service_mileage),
             battery.SpindlerBattery(last_service_date, current_date)
         )
 
-    def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    @staticmethod
+    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
         return Car(
             engine.WilloughbyEngine(current_mileage, last_service_mileage),
             battery.SpindlerBattery(last_service_date, current_date)
         )
 
-    def create_rorschach(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    @staticmethod
+    def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
         return Car(
             engine.WilloughbyEngine(current_mileage, last_service_mileage),
             battery.NubbinBattery(last_service_date, current_date)
         )
 
-    def create_thovex(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    @staticmethod
+    def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
         return Car(
             engine.CapuletEngine(current_mileage, last_service_mileage),
             battery.NubbinBattery(last_service_date, current_date)
         )
 
-    def create_palindrome(self, current_date, last_service_date, warning_light_on):
+    @staticmethod
+    def create_palindrome(current_date, last_service_date, warning_light_on):
         return Car(
             engine.SternmanEngine(warning_light_on),
             battery.SpindlerBattery(last_service_date, current_date)
